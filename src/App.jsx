54a992c3b338b2e60bd8feca31b300373a4c23fb
AdityaSri11 <srikant2@purdue.edu>
2022-03-10 21:38:55 -0500

    trial

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Topbar from "./components/topbar/Topbar";
+import TopBar from "./components/topbar/Topbar";
+import Intro from "./components/intro/Intro";
+import "./app.scss"
+
+function App() {
+  return (
+    <div className="app">
+      <Topbar/>
+      <div className="sections">
+        <Intro/>  
+      </div>
+    </div>
+  );
+}
+
+export default App;
