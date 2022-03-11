961aae230b59ec02662d4da790be8bb596bac105
AdityaSri11 <srikant2@purdue.edu>
2022-03-11 04:39:56 -0500

    blinkinking

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,14 +1,28 @@
 import React from "react";
-import "./intro.scss"
+import "./intro.scss";
+import { init } from 'ityped';
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
+
+    const textRef = useRef();
+
+    useEffect(() => {
+        init(textRef.current, {
+            showCursor: false,
+            backDelay: 1500,
+            backspeed: 60, 
+            strings: ["Student" , "Software Engineer", "Developer", "Techie"],
+        });
+    }, [])
+
     return (
         <div className="intro" id="intro">
             <div className="left">
                 <div className="wrapper">
                     <h2>Nice to meet you, I'm</h2>
-                    <h1>Aditya Srikanth</h1>
-                    <h3>Dedicated <span></span></h3>
+                    <h1>Aditya Srikanth,</h1>
+                    <h3>Dedicated <span ref ={textRef}></span></h3>
                 </div>
                 <a href="#experience">
                     <img src="assets/caret.png" alt=""/>    
