ac5d89f962f0bd3bf8a653f8b7e18a5a8d65db6a
AdityaSri11 <srikant2@purdue.edu>
2022-03-11 14:26:26 -0500

    Webpage

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -2,19 +2,40 @@ import React from "react";
 import ExperienceList from "../experienceList/ExperienceList";
 import { useEffect, useState } from "react";
 import "./experience.scss"
+import { projectsExperience, workExperience, extracurricularExperience } from "../../data";
 
 export default function Experience() {
 
     const[selected,setSelected] = useState("featured")
+    const[data,setData] = useState([])
 
-    const list = [ {id:"featured", title:"Featured",}, 
-    {id:"projects", title:"Projects",}, 
-    {id:"work", title:"Work",}, 
-    {id:"extracurriculars", title:"Extracurriculars",}, ];
+
+    const list = [
+        {id:"projects", title:"Projects",}, 
+        {id:"work", title:"Work",}, 
+        {id:"extracurriculars", title:"Extracurriculars",}, ];
+
+    useEffect(() => {
+
+        switch(selected){
+            case "projects":
+                setData(projectsExperience);
+                break;
+            case "work":
+                setData(workExperience);
+                break;
+            case "extracurriculars":
+                setData(extracurricularExperience);
+                break;
+            default:
+                setData(projectsExperience);
+        }
+
+    }, [selected])
 
     return (
         <div className="experience" id="experience">
-            <h1>Experience</h1>
+            <h1>Experience:</h1>
             <ul>
                 {list.map((item) => (
                     <ExperienceList title={item.title} active = {selected === item.id} setSelected = {setSelected}
@@ -22,22 +43,13 @@ export default function Experience() {
                 ))}
             </ul>
             <div className="container">
+                {data.map((d) => (
                 <div className="item">
-                    <img src="assets/vip-web.jpeg" alt="" />
-                    <h3>Vertically Integrated Project</h3>
+                    <img src= {d.img} alt="" />
+                    <h3>{d.title}</h3>
+                    
                 </div>
-                <div className="item">
-                    <img src="assets/vip-web.jpeg" alt="" />
-                    <h3>Vertically Integrated Project</h3>
-                </div>
-                <div className="item">
-                    <img src="assets/vip-web.jpeg" alt="" />
-                    <h3>Vertically Integrated Project</h3>
-                </div>
-                <div className="item">
-                    <img src="assets/vip-web.jpeg" alt="" />
-                    <h3>Vertically Integrated Project</h3>
-                </div>
+                ))}
             </div>
         </div>
     ) 
---
+++ src/data.js
@@ -0,0 +1,38 @@
+export const projectsExperience = [
+    {
+        id: 1,
+        title: "Vertically Integrated Projects",
+        img: "assets/vip-web.jpeg",
+    },
+    {
+        id: 2,
+        title: "Huffman Encoding Project",
+        img: "https://media.geeksforgeeks.org/wp-content/uploads/Untitled-Diagram-2-7.png",
+    },
+]
+
+export const workExperience = [
+    {
+        id: 1,
+        title: "General Atomics Summer 2022 Internship",
+        img: "https://www.makconstructiongf.com/wp-content/uploads/GeneralAtomics.jpg",
+    },
+    {
+        id: 2,
+        title: "Peer Teacher's Assistant: Python for Data Science",
+        img: "https://dz2cdn1.dzone.com/storage/temp/13975571-main-image-python-1.jpeg",
+    },
+]
+
+export const extracurricularExperience = [
+    {
+        id: 1,
+        title: "Boiler Consulting Group",
+        img: "https://engineering.purdue.edu/Engr/Academics/Undergraduate/PEPC/PEPC-Groups/BCG/BCG-web.jpg",
+    },
+    {
+        id: 2,
+        title: "Purdue Solar Car",
+        img: "https://images.squarespace-cdn.com/content/v1/53584784e4b0b772765f9073/1398662622947-TP4IHRNJK7FIDR5E3UOV/image-asset.jpeg",
+    },
+]
